17506dbdc95123722a0e8c0f565b99e74727d2ae
Dmitry <sicily007@rambler.ru>
2023-09-07 22:52:24 +0300

    sem7

---
+++ example_057/Program.cs
@@ -0,0 +1,155 @@
+﻿/*
+Задача 57: Составить частотный словарь элементов двумерного массива.
+Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+*/
+
+var array = GenArray();
+PrintArray(array);
+Console.WriteLine();
+
+Dictionary<int, int> dict = new Dictionary<int, int>();
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (dict.ContainsKey(array[i, j]))
+        {
+            dict[array [i, j]]++;
+        }
+        else
+        {
+            dict.TryAdd(array[i, j], 1);
+        }
+    }
+}
+
+foreach (var item in dict)
+{
+    Console.WriteLine($"Элемент {item.Key} Количество {item.Value}");
+}
+
+int[,] GenArray(int rows = 5, int cols = 5)
+{
+    Random random = new Random();
+    int[,] array = new int[rows, cols];
+    
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            array[i, j] = random.Next(1, 50);
+        } 
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+   
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+    Console.WriteLine();
+        
+    }
+}
+
+/*
+//Задайте двумерный массив. Напишите программу, 
+//которая поменяет местами первую и последнюю строку массива.
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+WriteLine();
+int[] rowAr=GetRowArray(array);
+SortArray(rowAr);
+WriteLine(String.Join(" ",rowAr));
+PrintData(rowAr);
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+int[] GetRowArray(int[,] inArray)
+{
+    int[] result = new int[inArray.GetLength(0)*inArray.GetLength(1)];
+    int index=0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            result[index] = inArray[i,j];
+            index++;
+        }
+    }
+    return result;
+}
+
+void SortArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        for (int j = i+1; j < inArray.Length; j++)
+        {
+            if(inArray[i]>inArray[j])
+            {
+                int k=inArray[i];
+                inArray[i]=inArray[j];
+                inArray[j]=k;
+            }
+        }
+    }
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+void PrintData(int[] inArray)
+{
+    int el=inArray[0];
+    int count = 1;
+    for (int i = 1; i < inArray.Length; i++)
+    {
+        if(inArray[i]!=el)
+        {
+            WriteLine($"{el} встречается {count}");
+            el=inArray[i];
+            count=1;
+        }
+        else
+        {
+            count++;
+        }
+    }
+    WriteLine($"{el} встречается {count}");
+}
+*/
